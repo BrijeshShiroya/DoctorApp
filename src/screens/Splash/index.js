feea7b11e322f976186a481b103a9e77fd7d22f5
Brijesh Shiroya <brijesh@creolestudios.com>
2018-11-20 18:22:12 +0530

    Add initial librarieslike redux, redux-thunk, react-navigation, react-redux, react-navigation-redux-helpers, axios

---
+++ src/screens/Splash/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import styles from './style';
+
+export default class Splash extends Component {
+    render() {
+        return (
+            <View style={styles.container} />
+        );
+    }
+}
